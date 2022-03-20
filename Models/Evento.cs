a30eb18bb5bdf08cd6a0b4cdea06b5f84fb8b539
KalianePemi <kaliane.pemi@gmail.com>
2022-03-20 20:15:11 -0300

    Criacao Model Evento

---
+++ Models/Evento.cs
@@ -0,0 +1,9 @@
+namespace Philotes.Models
+{
+    public class Evento
+    {
+         public System.DateTime Data {get;set;}
+         public Localizacao Localizacao {get; set;}
+         public float Recompensa {get; set;}
+    }
+}
