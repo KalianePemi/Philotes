34c4bbabe45dba8128f51fa06b7c92ffef8a7860
KalianePemi <kaliane.pemi@gmail.com>
2022-03-20 20:14:51 -0300

    Criacao Model Localizacao

---
+++ Models/Localizacao.cs
@@ -0,0 +1,12 @@
+namespace Philotes.Models
+{
+    public class Localizacao
+    {
+        string Endereco {get; set;}
+        string Cidade {get; set;}
+        string Estado {get; set;}
+        int Num {get; set;}
+        int Cep {get; set; }
+
+    }
+}
