c2b925d6ad89746b6e982cb585602bb32e9f99b4
MatheusDias <MatheusDias@gmail.com>
2022-03-22 22:36:27 -0300

    insert data base

---
+++ Data/DataContext.cs
@@ -0,0 +1,23 @@
+
+using Microsoft.EntityFrameworkCore;
+using Philotes.Models;
+
+namespace Philotes.Data
+
+{
+    public class DataContext : DbContext
+    {
+      //  public DataContext(DbContextOptions<DataContext> options): base(options)
+     //   {
+
+     //   }
+     //   public DbSet<Pet> Pets {get; set;}
+        
+     //   protected override void OnModelCreating(ModelBuilder modelBuilder)
+      //  modelBuilder.Entity<Pet>().HasData
+      //  (
+            //Inserir as linhas " new Pet() {Id = 2,..." da lista de Pets}
+      //       new Pet {Id = 5, Nome = "Chico", Raca = "SRD", Cores = new List<CorEnum>{CorEnum.Cinza}, Descricao = "O Gato mais de boa", Porte=PorteEnum.P, Sexo=SexoEnum.Masculino }
+       // );
+    }
+}
